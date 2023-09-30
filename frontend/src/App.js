834401817a79480e6abd508a41af3d9c59f24db4
CodeBurner <codeburner0@gmail.com>
2023-09-30 10:46:20 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Login from './Components/Login'
+// import Login from './Components/Login'
+import Signup from './Components/Signup'
 function App() {
   return (
     <div>
-      <Login/>
+      <Signup/>
     </div>
   )
 }
