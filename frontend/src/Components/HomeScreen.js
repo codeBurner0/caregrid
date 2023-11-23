9c62543bf436a8ae5cfc4456f9d3b48ecf912121
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2023-11-23 21:20:52 +0530

    frontend

--- frontend/src/Components/HomeScreen.js
+++ frontend/src/Components/HomeScreen.js
@@ -7,6 +7,7 @@ import doctors from "../assets/images/doctor-bg.png"
 import design1 from '../assets/images/design1.png'
 import design2 from '../assets/images/design2.png'
 import design3 from '../assets/images/design3.png'
+import downdoctor from '../assets/images/down-doctor.png'
 
 function HomeScreen() {
 
@@ -39,15 +40,32 @@ function HomeScreen() {
         <div className='sec2'>
           <h1>The Best Quality Services You Can Get</h1>
           <div className="inner">
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
+            <div className="card">
+              <img src="https://images.pexels.com/"/>
+              <h2>Hostipals</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing el </p>
+              <button>Know this more</button>
+            </div>
+            <div className="card">
+               <img src="https://images.pexels.com/"/>
+              <h2>Doctors</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing el </p>
+              <button>Know this more</button>
+            </div>
+            <div className="card">
+              <img src="https://images.pexels.com/"/>
+              <h2>Medicines</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing el </p>
+              <button>Know this more</button>
+            </div>
           </div>
         </div>
 
 
         <div className='sec3'>
-            
+            <div className="sec3-img">
+              <img src={downdoctor} alt="" />
+            </div>
         </div>
 
         <div className='sec4'>
