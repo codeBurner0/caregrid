048bedfee654b67dbdd357c4accdc1ecde651383
CodeBurner <codeburner0@gmail.com>
2023-11-26 17:56:25 +0530

    navbar added

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,47 +1,62 @@
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom'
-// import {GiHamburgerMenu} from 'react-icons/gi'
-import { RiMenuFoldLine } from "react-icons/ri";
-import '../assets/styles/NavBar.css'
-import logo from "../assets/images/CareGrid_logo.png"
-import login from '../assets/images/login.png'
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BsFillMenuButtonFill } from 'react-icons/bs';
+import { BsFillMenuButtonWideFill } from 'react-icons/bs';
+import Logo from '../assets/images/Caregrid_logo.png'
+import "../assets/styles/NavBar.css";
 function NavBar() {
+  const [bool, setBool] = useState(false);
+  function clicker() {
+    setBool(!bool);
+  }
+  const navigate = useNavigate()
 
-    const [click, setClick] = useState(false);
-    const [isLogin,setisLogin] = useState(false) ; 
-   return (
-    <nav className="nb-parent">
-        <div className='nb-child'>
-            <a className="nb-part1" href='/home'>
-            <img src={logo} alt="" />
-                <div className='div1'>CareGrid</div>
-            </a>
-            <div className={!click ?"nb-part2":"nb-part21"}>
-              <li><Link to='/appointment'>Appointment</Link></li>
-              <li><Link to='/beds'>Beds</Link></li>
-              <li><Link to='/medicines'>Medicines <span>{' '}</span></Link></li>
-              {/* <li><Link to='/about' className='green-in'>About</Link></li> */}
-              <li><Link to='/contact' className='green-in'>Contact</Link></li>
-              <div className='nav-btn'>
-              {isLogin ? 
-                  <Link to={'/profile'}>Profile</Link>:
-                   <Link to={'/login'}><img src={login} alt="" /></Link>}
-                
-              </div>
-             
-            </div>
-            
+ 
+
+
+
+  return (
+    <div>
+      <div className={"nav-bar"}>
+        <div className="title">
+          <img src={Logo} alt="" className="nav_logo" />
+          <span className="logo"><Link className="logo1" to="/">
+            CareGrid
+          </Link></span>
+          {(bool) ? <BsFillMenuButtonFill className="icon" onClick={() => clicker()} /> : <BsFillMenuButtonWideFill className="icon" onClick={() => clicker()} />}
         </div>
-        <div class="hamburger-menu">
-                <a href="#" onClick={()=>{setClick(!click)}}>
-                   <RiMenuFoldLine />
-                </a>
-         </div>
-    </nav>
-  
-    
-  )
+        <div className={bool ? "show" : "list"}>
+          <li>
+            <Link className="anchor" to="/home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="anchor" to="/appointment">
+              Appointment
+            </Link>
+          </li>
+          <li>
+            <Link className="anchor" to="/facility">
+              Facility
+            </Link>
+          </li>
+          <li>
+            <Link className="anchor" to="/createblog">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="nav_schedule">
+              Schedule
+            </Link>
+          </li>
+          <li>
+            <button className='nav_login_button'><span>Login</span></button>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default NavBar
+export default NavBar;
